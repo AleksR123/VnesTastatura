c49c5e9ee38c32eb206cdd7aaf7405b9b6ecc6a0
unknown <aleksandarristov28@gmail.com>
2023-12-23 19:00:53 +0100

    first java program

---
+++ src/Vnes.java
@@ -0,0 +1,11 @@
+	import java.util.Scanner;
+
+	public class Vnes {
+
+		public static void main(String[] args) {
+			
+	      Scanner myVar=new Scanner(System.in);
+	      System.out.println(myVar.nextLine());
+			
+		}
+}
